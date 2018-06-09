4e22361c712fb2ff4d7896c638cf7c0f7ce21719
Christos Skretas <christos.skretas@gmail.com>
2018-06-09 06:16:47 +0300

    finished the program

--- lib/translations.rb
+++ lib/translations.rb
@@ -4,10 +4,15 @@ require 'json'
 require 'csv'
 require 'roo'
 
-
 class Translations
 
+
+  def initialize()
+    puts "initializing translations"
+  end
+
   def self.export lang
+    #Create an object from ./app/assets/#{lang}.json and return it
     data = JSON.parse(File.read("./app/assets/#{lang}.json"));
 
 
@@ -97,7 +102,9 @@ class Translations
   end
 
   def self.compareAndImport lang, file
-    compare lang, file
+    #It will compare the xls we provide with the current ./app/assets/lang.json files and it will export new ones
+    #The new files will also have the added keys with their values, or the changed values.
+
     xls_hash = read_xls lang, file
     data_hash = export lang
     data_hash = data_hash.reduce Hash.new, :merge
@@ -116,6 +123,97 @@ class Translations
     File.open("./app/assets/temp"+lang+".json","w") do |f|
       f.write(getToWriteToFile.to_json)
     end
+
+  end
+
+  def self.compareAndExportToConsole file
+
+    langs = ["en","el","de"]# πρεπει να το κάνω να τα παίρνει απο την πρώτη σειρα
+    multi_lingual_data_hash = {}
+    multi_lingual_xls_hash = {}
+    langs.each do |item|
+      data_hash = export item
+      data_hash = data_hash.reduce Hash.new, :merge
+      xls_hash = read_xls item, file
+      multi_lingual_data_hash[item] = data_hash
+      multi_lingual_xls_hash[item] = xls_hash
+    end
+    
+    temp_xls_changed = {}
+    temp_xls_added = {}
+
+    puts "Checking if apps translation files are different from the main xls. If yes we will export new xls with the differences."
+    multi_lingual_data_hash.each do |lang,hash|
+      hash.each do |key, value|
+
+        if multi_lingual_xls_hash[lang][key] != value && multi_lingual_xls_hash[lang][key] != nil
+          if temp_xls_changed[key] == nil
+            temp_xls_changed[key] = {}
+            multi_lingual_data_hash.each do |lang,hash|
+              temp_xls_changed[key][lang] = 'nochange'
+            end
+          end
+
+          temp_xls_changed[key][lang] = multi_lingual_xls_hash[lang][key].to_s + '=>' + value.to_s
+        elsif  multi_lingual_xls_hash[lang][key] == nil
+          if temp_xls_added[key] == nil
+            temp_xls_added[key] = {}
+            multi_lingual_data_hash.each do |lang,hash|
+              temp_xls_added[key][lang] = 'nochange'
+            end
+          end
+
+          temp_xls_added[key][lang] = value.to_s
+        end
+
+      end
+    end
+    
+
+
+
+    3.times do 
+      puts "Values that have been CHANGED"
+    end
+    
+    temp_xls_changed.each do |key, value|
+      array = []
+      value.each do |key, value|
+        array.push(value)
+      end
+      array = array.join('$')
+
+
+      puts key.to_s + "$" + array.to_s
+    end
+
+    3.times do 
+      puts "Values that have been CHANGED"
+    end
+
+    3.times do 
+      puts "Values that have been ADDED"
+    end
+
+    temp_xls_added.each do |key, value|
+      array = []
+      value.each do |key, value|
+        array.push(value)
+      end
+      array = array.join('$')
+
+
+      puts key.to_s + "$" + array.to_s
+    end
+
+    3.times do 
+      puts "Values that have been ADDED"
+    end
+
+    #getToWriteToFile = exportNewJsonFiles data_hash
+    #File.open("./app/assets/temp"+lang+".json","w") do |f|
+      #f.write(getToWriteToFile.to_json)
+    #end
 
   end
 
